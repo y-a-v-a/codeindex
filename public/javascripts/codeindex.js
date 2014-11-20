bf8d7476efc156c2db6ccaab84e9ddcad24a059e
Vincent Bruijn <vebruijn@gmail.com>
2014-11-21 00:07:59 +0100

    more features added

--- public/javascripts/codeindex.js
+++ public/javascripts/codeindex.js
@@ -1,30 +1,62 @@
 $(function() {
-    var i = $('input');
+    var i = $('input[name="query"]');
     i.on('keyup', function() {
-        mydelay(function() {
+        delay(function() {
             var val = $('input').val();
             if (val.length >= 3) {
                 $.getJSON('/query/' + val)
                 .done(function(resp) {
-                    var d = $('.results').empty();
+                    var results = $('.results').empty();
                     var data = resp;
                     data.forEach(function(el) {
-                        var a = $('<div/>', {'class': 'result'});
-                        a.append($('<div/>').text(el.path_t));
-                        a.append($('<pre/>').append($('<code/>', {'class': 'language-javascript'}).text(el.content_t)));
-                        d.append(a);
+                        var result = $('<div/>', {'class': 'result'});
+                        var path = $('<div/>').text(el.path_t);
+                        var code = $('<pre/>').append($('<code/>', {'class': 'language-javascript'}).text(el.content_t));
+                        result.append(path);
+                        result.append(code);
+                        results.append(result);
                     });
                 }).always(function() {
-                    // Prism.highlightAll();
+                    Prism.highlightAll();
                 });
             } else {
                 $('.result').empty();
             }
         }, 600);
     });
+    
+    var j = $('input[type="button"]');
+    j.on('click', function() {
+        $('.message').text('').fadeIn();
+        j.attr('disabled', true);
+
+        var path = $('input[name="index"]').val();
+        if (path.length < 5) {
+            $('.message').text('Path too short');
+            reset();
+            return;
+        }
+        $.ajax('/doindex', { type: 'POST', data: { path : path }})
+        .done(function(data) {
+            $('.message').text(data);
+            $('input[name="index"]').val('');
+            i.focus();
+        }).fail(function(xhr, type, msg) {
+            $('.message').text(xhr.responseText);
+        }).always(function() {
+            reset();
+        });
+    });
+    
+    function reset() {
+        j.attr('disabled', false);
+        setTimeout(function() {
+            $('.message').fadeOut();
+        }, 5000);
+    }
 });
 
-var mydelay = (function() {
+var delay = (function() {
   var timer = 0;
   return function(callback, ms){
     clearTimeout (timer);
--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,25 @@ var express = require('express');
 var router = express.Router();
 var solr = require('../lib/solr');
 
-/* GET home page. */
 router.get('/', function(req, res) {
     res.render('index', { title: 'Code Search' });
 });
 
-router.get('/query/:query', function(req, res){
-    // console.log(req.result);
+router.get('/query/:query', function(req, res) {
     res.send(req.result);
-    // res.render('index', { title: 'Express' });
+});
+
+router.post('/doindex', function(req, res) {
+    var path = req.body.path || '';
+    solr.addFromPathToIndex(path, function(err) {
+        if (err) {
+            res.status(422).send(err.message);
+        }
+        res.status(200).send('Done reindexing ' + path);
+    });
 });
 
 module.exports = router;
-
 
 router.param('query', function(req, res, next, query) {
     solr.queryIndex(query, function(err, result) {
