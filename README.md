c410617efb81a39e0864a1f74282615810901f12
Vincent Bruijn <vebruijn@gmail.com>
2014-11-26 21:59:30 +0100

    made Angluar frontend shit

--- README.md
+++ README.md
@@ -3,15 +3,15 @@ codeindex
 
 wrapper for solr to index code on your local machine
 
-Best tokenizer I found to be for JavaScript is this:
+Best analyzer configuration I found to be for JavaScript is this:
 
 ```xml
 <analyzer type="index">
-    <tokenizer class="solr.PatternTokenizerFactory" pattern="[ \.\(\)\[\]\-;\'&quot;\\/]" group="-1"/>
+    <tokenizer class="solr.StandardTokenizerFactory"/>
     <filter class="solr.EdgeNGramFilterFactory" minGramSize="3" maxGramSize="24"/>
     <filter class="solr.LowerCaseFilterFactory"/>
 </analyzer>
 ```
 
-<code>group="-1"</code> means the given regular expression is considered a delimiter-definition.
+<code>group="-1"</code> means the given regular expression is considered a delimiter-definition. The NGram filter will chop all tokens into parts from 3 to, if present 24 characters in length. This helps in finding partial results, which makes matching, and searching faster.
 
---
+++ public/javascripts/controllers.js
@@ -0,0 +1,43 @@
+// ng
+var codeIndexApp = angular.module('codeIndexApp', []);
+
+codeIndexApp.controller('CodeIndexCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.fetch = function() {
+        $http.get('/query/' + encodeURIComponent($scope.text))
+        .success(function(data) {
+            $scope.highlighting = (postProcess(data)).highlighting;
+        });
+    }
+
+    $scope.doindex = function() {
+        $http.post('/doindex', { path: $scope.path })
+        .success(function(msg, status) {
+            $scope.message = msg;
+            $scope.path = '';
+        })
+        .error(function(msg, status) {
+            $scope.message = msg;
+            $scope.path = '';
+        });
+    }
+}]);
+
+codeIndexApp.filter("sanitize", ['$sce', function($sce) {
+    return function(htmlCode){
+        return $sce.trustAsHtml(htmlCode);
+    }
+}]);
+
+// custom
+function postProcess(data) {
+    var key;
+    if (data.response && data.response.docs) {
+        for(var i = 0; i < data.response.docs.length; i++) {
+            key = data.response.docs[i].id;
+            if (typeof data.highlighting[key] !== 'undefined') {
+                data.highlighting[key].path_t = data.response.docs[i].path_t;
+            }
+        }
+    }
+    return data;
+}
